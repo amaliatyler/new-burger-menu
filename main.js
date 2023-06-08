96c5a4c69edca1373f4e2adebc7581bd4c18a043
Амалия Тайлер <AATayler@1cbit.ru>
2023-06-08 12:05:49 +0300

    finished burder-menu + header

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 'use strict'
-
-'use strict';
+ /* автор кода - Евгений Андриканич */
 
 const isMobile = {
     Android: function () {
@@ -39,7 +38,7 @@ if (isMobile.any()) {
         for (let i = 0; i < menuArrows.length; i++) {
             const menuArrow = menuArrows[i];
             menuArrow.addEventListener('click', function (e) {
-                /*тогглим класс активности у родителя */
+                /*тогглим класс активности у родителя нажатой стрелочки */
                 menuArrow.parentElement.classList.toggle('_active');
             });
         }
@@ -55,36 +54,50 @@ const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
 if(iconMenu) {
     iconMenu.addEventListener('click', function(e) {
+        /* блокируем возможность взаимодействия со страницей при открытом мобильном меню */
         document.body.classList.toggle('_lock');
+        /* добавляем класс активности к бургеру для анимирования */
         iconMenu.classList.toggle('_active');
+        /* добавляем класс активности для меню, чтобы оно выезжало */
         menuBody.classList.toggle('_active');
     });
 }
 
 /* прокрутка при клике */
 
+/* создаем массив ссылок, у которых есть дата-атрибут goto*/
 const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+/* если такие существуют */
 if(menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
+        /* на каждую из них по клику вешаем функцию */
         menuLink.addEventListener('click', onMenuLinkClick);
     });
 
     function onMenuLinkClick(e) {
+        /* получаем объект, на котором произошел клик */
         const menuLink = e.target;
+        /* проверяем, заполнен ли дата-атрибут и существует ли объект, на который ссылается данный дата-атрибут */
         if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            /* получаем в константу сам объект */
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            /* получаем положение элемента на странице сверху, суммируем с положением прокрутки и вычитаем высотку хэдера, тк шапка в положении fixed */
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('header').offsetHeight;
         
+            /* если иконка бургера активна, убираем класс _lock и классы активности */
             if(iconMenu.classList.contains('_active')) {
                 document.body.classList.remove('_lock');
                 iconMenu.classList.remove('_active');
                 menuBody.classList.remove('_active');
             }
 
+            /* обращаемся к объекту окна,  */
             window.scrollTo({
+                /* указываем откуда и насколько прокрутить окно */
                 top: gotoBlockValue,
                 behavior: 'smooth'
             });
+            /* выключаем дефолтное поведение, чтобы ссылка плавно переходила к разделу вместо перехода в новую вкладку */
             e.preventDefault();
         }
     }
